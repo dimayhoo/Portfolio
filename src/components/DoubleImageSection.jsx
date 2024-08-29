66d007451dc5c73ef9c9cc491b3208a3a69e64e4
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-29 22:26:01 +0500

    Added photoes for projects

--- src/components/DoubleImageSection.jsx
+++ src/components/DoubleImageSection.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 
 function DoubleImageSection({data}) {
+  const positions = {
+    'center': 'items-center justify-center p-8',
+    'x-center': 'items-end justify-center px-8 pt-8',
+    'bottom-left': 'items-end justify-start pr-8 pt-8',
+    'bottom-right': 'items-center justify-end pl-8 pt-8'
+  }
+
+  const backgrounds = {
+
+  }
+
   return (
     <section className='flex flex-col space-y-16 px-14 h-[200vh]'>
-        <div className='rounded-xl flex-1'>
-            <div>
-                <img alt="image from project" />
-            </div>
+        <div className='project-image-wrapper'>
+            <img alt="image from project" className='project-image'/>
         </div>
-        <div className='flex space-x-16 rounded-xl'>
+        <div className='flex space-x-16 project-image-wrapper'>
             <div className='flex-1'>
-                <div>
-                    <img alt="image from project" />                
-                </div>                
+                <img alt="image from project" className='project-image'/>                                
             </div>
             <div className='flex-1'>
-                <div>
-                    <img alt="image from project" />
-                </div>
+                <img alt="image from project" className='project-image'/>
             </div>
         </div>
-        <div className='rounded-xl flex-1'>
-            <div>
-                <img alt="image from project" />
-            </div>
+        <div className='project-image-wrapper'>
+            <img alt="image from project" className='project-image'/>
         </div>
     </section>
   )
