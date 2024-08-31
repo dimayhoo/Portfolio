cccef33ffe1573fca44af8b479e6b87c3ce15431
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-31 09:46:56 +0500

    Added missing software logoes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import data from './data.json';
 import Expertise from './components/Expertise.jsx';
 import Contact from './components/Contact.jsx';
 
+// qq Storing assets images folder in order to use links to the items inside it form other files on deployment. What is the problem at all? What are the two solutions? 
 export default function App() {
   // qq What is the best way to handle outside clicks using web popover api?
 
